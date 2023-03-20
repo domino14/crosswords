bbd5dc3abea477ed5d173cbc3a89f22cb8cdb090
César Del Solar <delsolar@gmail.com>
2023-03-19 21:33:10 -0400

    add a failing test

--- pkg/cwgame/game_test.go
+++ pkg/cwgame/game_test.go
@@ -1,1 +1,18 @@
 package cwgame
+
+import (
+	"testing"
+
+	"github.com/domino14/liwords/pkg/cwgame/runemapping"
+	"github.com/matryer/is"
+)
+
+func TestLeave(t *testing.T) {
+	// Test blank exchange works
+	is := is.New(t)
+	l, err := Leave([]runemapping.MachineLetter{0, 3, 5, 6},
+		[]runemapping.MachineLetter{0})
+
+	is.NoErr(err)
+	is.Equal(l, []runemapping.MachineLetter{3, 5, 6})
+}
